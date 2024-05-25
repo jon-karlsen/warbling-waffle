6de6460bfe0858b32d6b600f6e2ee3440d3588fe
Jon Karlsen <karlsen.jon@icloud.com>
2024-05-25 20:41:43 +0900

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+use tracing::info;
+use tracing_subscriber::{ layer::SubscriberExt , util::SubscriberInitExt };
+
+
+#[ tokio::main ]
+async fn main() {
+    tracing_subscriber::registry()
+        .with( tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(| _ | "warbling_waffle=debug".into() ) )
+        .with( tracing_subscriber::fmt::layer() )
+        .init();
+
+    info!( "Hello, WarblingWaffle!" );
+}
