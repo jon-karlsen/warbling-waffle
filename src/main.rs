407dcd693d72eccf4a7cca88736e0effd0de0ab2
Jon Karlsen <karlsen.jon@icloud.com>
2024-05-25 21:08:01 +0900

    feat: test view

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,32 @@
+use axum::{ routing::get , Router };
 use tracing::info;
 use tracing_subscriber::{ layer::SubscriberExt , util::SubscriberInitExt };
 
 
+mod view;
+
+
+use crate::view::hello;
+
+
 #[ tokio::main ]
-async fn main() {
+async fn main() -> anyhow::Result<()> {
     tracing_subscriber::registry()
         .with( tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(| _ | "warbling_waffle=debug".into() ) )
         .with( tracing_subscriber::fmt::layer() )
         .init();
 
-    info!( "Hello, WarblingWaffle!" );
+    info!( "Initialising app..." );
+
+    let app      = Router::new()
+                    .route( "/" , get( hello ));
+    let listener = tokio::net::TcpListener::bind( "0.0.0.0:3000" ).await.unwrap();
+
+    info!( "Initialisation complete" );
+
+    axum::serve( listener , app ).await.unwrap();
+
+    info!( "Application shutting down" );
+
+    Ok( () )
 }
---
+++ src/view/mod.rs
@@ -0,0 +1,31 @@
+use askama::Template;
+use axum::{ http::StatusCode , response::{ Html , IntoResponse , Response } };
+
+
+struct HtmlTemplate<T>( T );
+
+
+impl<T> IntoResponse for HtmlTemplate<T>
+    where
+        T: Template {
+    fn into_response( self ) -> Response {
+        match self.0.render() {
+             Ok( h ) => Html( h ).into_response()
+          , Err( e ) => (
+                StatusCode::INTERNAL_SERVER_ERROR
+              , format!( "Failed to render template. Error: {}" , e )
+            ).into_response()
+        }
+    }
+}
+
+
+#[ derive( Template )]
+#[ template( path = "hello.html" )]
+struct HelloTemplate;
+
+
+pub async fn hello() -> impl IntoResponse {
+    let template = HelloTemplate {};
+    HtmlTemplate( template )
+}
